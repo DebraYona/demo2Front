2f40263ab8a7e1214da86cee5388d5f2d2100434
Cesar Gutierrez <cesar@tineo.mobi>
2018-10-20 05:12:19 -0500

    logout

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 
-const Header =(props)=>(
-  <header>
-            <div className="contenedor">
-                <h2 className="titulo-banco">BANCO</h2>
-                <button className="btn btn-primary btn-s">Salir <span class="icon-exit"> </span> </button>
+class Header extends Component {
+
+
+    logout(){
+        sessionStorage.clear();
+        window.location.reload();
+    }
+
+
+    render() {
+
+
+        return (
+            <header>
+                <div className="contenedor">
+                    <h2 className="titulo-banco">BANCO</h2>
+                    <button className="btn btn-primary btn-s" onClick={this.logout}>Salir <span class="icon-exit"> </span></button>
 
                 </div>
-      </header>
+            </header>
+        )
 
-
-)
+    }
+}
 export default Header
